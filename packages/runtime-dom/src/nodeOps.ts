df4d6feb2ca1b82d9d6f8bee975e058ebd9acbf8
v_litongxin01 <v_litongxin01@baidu.com>
2024-12-17 18:33:59 +0800

    complate nodeOps

--- packages/runtime-dom/src/nodeOps.ts
+++ packages/runtime-dom/src/nodeOps.ts
@@ -1,1 +1,23 @@
-export const nodeOps = {}
+export const nodeOps = {
+    insert: (child, parent, anchor) => {
+        parent.insertBefore(child, anchor || null)
+    },
+    remove: child => {
+        const parent = child.parentNode
+        if (parent) {
+            parent.removeChild(child)
+        }
+    },
+    createElement: tag => document.createElement(tag),
+    createText: text => document.createTextNode(text),
+    createCommon: comment => document.createComment(comment),
+    setText: (node, text) => {
+        node.nodeValue = text
+    },
+    setElementText: (el, text) => {
+        el.textContent = text
+    },
+    parentNode: node => node.parentNode,
+    nextSibling: node => node.nextSibling,
+    querySelector: (el, selector) => el.querySelector(selector),
+}
