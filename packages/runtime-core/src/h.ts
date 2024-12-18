85f175c113d49d5174d13731db9ca2b57c450e32
v_litongxin01 <v_litongxin01@baidu.com>
2024-12-18 12:58:10 +0800

    complete h

--- packages/runtime-core/src/h.ts
+++ packages/runtime-core/src/h.ts
@@ -14,13 +14,13 @@ export function h(type, propsOrChildren, children) {
 
     if (l === 2) {
         if (isObject(propsOrChildren) && !isArray(propsOrChildren)) {
-            if (isVnode(propsOrChildren)) {
-                return createVnode(type, null, [propsOrChildren]);
+            if (isVNode(propsOrChildren)) {
+                return createVNode(type, null, [propsOrChildren]);
             }
-            return createVnode(type, propsOrChildren);
+            return createVNode(type, propsOrChildren);
         }
         else {
-            return createVnode(type, null, propsOrChildren);
+            return createVNode(type, null, propsOrChildren);
         }
         
     }
@@ -29,11 +29,11 @@ export function h(type, propsOrChildren, children) {
         if (l > 3) {
             children = Array.prototype.slice.call(arguments, 2);
         }
-        else if (l === 3 && isVnode(children)) {
+        else if (l === 3 && isVNode(children)) {
             children = [children];
         }
 
-        return createVnode(type, propsOrChildren, children);
+        return createVNode(type, propsOrChildren, children);
     }
 
 }
